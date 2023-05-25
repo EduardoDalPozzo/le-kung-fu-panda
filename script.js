439cb6a06c81ed831ad2593da3ed44b0f361e3f1
Du <110917469+EduardoDalPozzo@users.noreply.github.com>
2023-05-25 09:01:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 
-var nome = window.prompt('teu nome?')
-window.alert ('é um prazer de conhecer, ' + nome + '!')
-var n1= Number.parseInt(window.prompt('digite seu número'))
-var n2= Number.parseInt(window.prompt('digite seu número'))
+var nome= (window.prompt('teu nome?'))
+var nasceu= (window.prompt('nasceu quando?'))
+var idade= (window.prompt('tua idade?'))
+var email= (window.prompt('teu email?'))
+var CPF= (window.prompt('teu CPF?'))
+
+
+window.alert ('o individuo : ' + nome + ',' + 'nasceu :' + nasceu + ','+'tem idade :' + idade +',' +'seu e-mail é :' + email + ','+'seu CPF :' + CPF + ',')
+
+
+
+
+
+
+var n1= Number.parseInt(window.prompt(' número'))
+var n2= Number.parseInt(window.prompt(' número'))
 var soma= n1 + n2 
 window.alert('o resultado da soma é: ' + soma + '!')
