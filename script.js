52947f4705a64ed99c0cf875dbd7bf1010958e62
Du <110917469+EduardoDalPozzo@users.noreply.github.com>
2023-05-25 08:16:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var nome = window.prompt('teu nome?')
+window.alert ('é um prazer de conhecer, ' + nome + '!')
+var n1= Number.parseInt(window.prompt('digite seu número'))
+var n2= Number.parseInt(window.prompt('digite seu número'))
+var soma= n1 + n2 
+window.alert('o resultado da soma é: ' + soma + '!')
