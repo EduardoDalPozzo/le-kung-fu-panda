282c0d664b3d89d236896b27634ea15b79541267
Du <110917469+EduardoDalPozzo@users.noreply.github.com>
2023-06-01 09:08:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var nome= (window.prompt('teu nome?'))
+/*var nome= (window.prompt('teu nome?'))
 var nasceu= (window.prompt('nasceu quando?'))
 var idade= (window.prompt('tua idade?'))
 var email= (window.prompt('teu email?'))
@@ -8,12 +8,21 @@ var CPF= (window.prompt('teu CPF?'))
 
 window.alert ('o individuo : ' + nome + ',' + 'nasceu :' + nasceu + ','+'tem idade :' + idade +',' +'seu e-mail é :' + email + ','+'seu CPF :' + CPF + ',')
 
-
-
-
-
-
 var n1= Number.parseInt(window.prompt(' número'))
 var n2= Number.parseInt(window.prompt(' número'))
 var soma= n1 + n2 
 window.alert('o resultado da soma é: ' + soma + '!')
+*/
+
+var n1 = Number.parseInt(window.prompt('Digite um número: '))
+var n2 = Number.parseInt(window.prompt('Digite outro número'))
+
+var soma= n1 + n2 
+var sub = n1 - n2
+var mult = n1*n2
+var div = n1/n2
+var pot = n1**n2
+
+
+window.alert('o resultado dessas operações são: soma:' + soma + '_____subtração:' + sub +
+'_____multiplicação:' + mult + '_____divisão:' + div + '_____potenciação:' + pot )
